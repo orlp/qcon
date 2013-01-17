7ad0af8a92e752798e63b8d49635e94e9c7b12f8
Orson Peters <orsonpeters@gmail.com>
2013-01-17 18:03:56 +0100

    more work

--- environment/pushd_cd_alias.bat
+++ environment/pushd_cd_alias.bat
@@ -3,8 +3,21 @@
 REM if no arguments were passed be have like the normal CD
 if [%1]==[] (
     chdir
-) else (
-    pushd %*
+    goto end
 )
 
-@echo on
+REM replace forward slashes with backward slashes
+set args=%1
+shift
+:start
+if [%1] == [] goto done
+set args=%args% %1
+shift
+goto start
+
+:done
+pushd %args:/=\%
+set args=
+
+:end
+echo on
--- environment/setup_environment.bat
+++ environment/setup_environment.bat
@@ -7,14 +7,13 @@ REM don't bother setting up an environment if we're not interactive
 echo %CMDCMDLINE% | find /i "/c" >nul
 if not errorlevel 1 goto exit
 
-set CMDRC_PATH=%~dp0
-set CONSOLE_NR=%1
+
 
 REM welcome message (first to promote the sense of being fast)
 for /f "tokens=1 delims=:" %%j in ('ping %computername% -4 -n 1 ^| findstr Reply') do (
     set localip=%%j
 )
-echo [%CONSOLE_NR%] %computername% (%localip:~11%)
+echo [%1] %computername% (%localip:~11%)
 
 
 REM startup dir
@@ -23,21 +22,30 @@ cd c:\
 REM always use pushd
 doskey dirs=pushd
 doskey pd=popd $*
-doskey cd=%CMDRC_PATH%pushd_cd_alias $*
+doskey cd=%~dp0pushd_cd_alias $*
 
 REM set up msls
-doskey ls=%CMDRC_PATH%msls\ls.exe $*
+doskey ls=%~dp0msls\ls.exe $*
+
+REM emulate a bit of linux
+doskey cat=type $*
+doskey mv=move $*
 
 REM enable ansi colors
 if "%PROCESSOR_ARCHITECTURE%"=="x86" (
-    %CMDRC_PATH%ansicon\x86\ansicon.exe -p
+    %~dp0ansicon\x86\ansicon.exe -p
 ) else (
-    %CMDRC_PATH%ansicon\x64\ansicon.exe -p
+    %~dp0ansicon\x64\ansicon.exe -p
 )
+
+REM environment variables
+endlocal
+set PATH=%PATH%;%~dp0bin
+set CONSOLE_NR=%1
 
 REM run clink (this HAS to be the last command, that's why we already put echo to on)
 echo on
-@%CMDRC_PATH%clink\clink.bat inject --quiet --profile %CMDRC_PATH%clink_settings --scripts %CMDRC_PATH%clink_scripts
+@%~dp0clink\clink.bat inject --quiet --profile %~dp0clink_settings --scripts %~dp0clink_scripts
 
 
 :exit
